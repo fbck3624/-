a157835699e9435008229f3b1e742f15ada52d2c
fbck3624 <52129117+fbck3624@users.noreply.github.com>
2021-08-10 01:39:14 +0800

    up
    up

--- Q1.php
+++ Q1.php
@@ -1,20 +1,28 @@
 <?php
-//今有車輛「汽車」和「機車」,請使用物件的繼承介面描述二者相同與差異,及二物件的執行程式碼 
+//今有車輛「汽車」和「機車」,請使用物件的繼承/介面描述二者相同與差異,及二物件的執行程式碼 
 
-//「汽車」與「機車」都是車，他們有相同的地方，因此可以透過interface定義相同的方法(vehicle)，
-//再宣告car和motorcycle作為子類別去繼承vehicle並實作方法，子類別亦可宣告自己的方法。
+//「汽車」與「機車」都是車，他們有相同的地方，因此可以透過interface定義相同的方法(vehicle)，再宣告car和motorcycle去實作vehicle的方法。
+//movement是移動速度，作為父類別，car和motorcycle為子類別去繼承movement，父類別已經寫好function，子類別繼承後可直接呼叫，亦可覆寫其function。
 interface vehicle{
 	public function color();
     public function oil();
     public function wheels();
 }
-class car implements vehicle {
-    private $color,$oil,$wheels,$window;
-    function __construct($c,$o,$wh,$w){
+class movement {
+    private $speed;
+    public function setSpeed($s){
+        $this->speed = $s;
+    }
+    public function getSpeed(){
+       return $this->speed;
+    }
+}
+class car extends movement implements vehicle {
+    private $color,$oil,$wheels;
+    function __construct($c,$o,$wh){
 		$this->color=$c;
 		$this->oil=$o;
         $this->wheels=$wh;
-        $this->window=$w;
 	}
     public function color(){
         return $this->color;
@@ -25,11 +33,8 @@ class car implements vehicle {
     public function wheels(){
         return $this->wheels;
     }
-    public function window(){
-        return $this->window;
-    }
 }
-class motorcycle implements vehicle {
+class motorcycle extends movement implements vehicle {
     private $color,$oil,$wheels;
     function __construct($c,$o,$wh){
 		$this->color=$c;
@@ -47,8 +52,12 @@ class motorcycle implements vehicle {
     }
     
 }
-$TOYOTA=new car('white',35,4,'matte');
+$TOYOTA=new car('white',35,4,'matte',5);
 echo $TOYOTA->oil()."</br>";
+$TOYOTA->setSpeed(130);
+echo $TOYOTA->getSpeed()."</br>";
 $GP=new motorcycle('black',3,2);
 echo $GP->wheels()."</br>";
+$GP->setSpeed(90);
+echo $GP->getSpeed()."</br>";
 ?>
