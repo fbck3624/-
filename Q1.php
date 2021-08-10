e8ceac51a39ef8616bafafd8b3db8e77dc2a7a7f
fbck3624 <52129117+fbck3624@users.noreply.github.com>
2021-08-10 09:45:55 +0800

    Update Q1.php

--- Q1.php
+++ Q1.php
@@ -3,6 +3,10 @@
 
 //「汽車」與「機車」都是車，他們有相同的地方，因此可以透過interface定義相同的方法(vehicle)，再宣告car和motorcycle去實作vehicle的方法。
 //movement是移動速度，作為父類別，car和motorcycle為子類別去繼承movement，父類別已經寫好function，子類別繼承後可直接呼叫，亦可覆寫其function。
+//繼承與介面差異:
+//繼承是子類別繼承父類別，或子介面繼承父介面，透過繼承可獲得父類別/父介面的function，並使用它，若有需要也可父寫。
+//介面不必實作function，其他class可透過implements實作介面function。
+//只能繼承一個類別，但可實作多個介面。
 interface vehicle{
 	public function color();
     public function oil();
